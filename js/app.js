0d9919feff0e6ce3c182d943c3ea6c37f35a3ea8
mamun102076 <mamun102076@gmail.com>
2022-08-22 23:03:29 +0600

    added budget dom element

--- js/app.js
+++ js/app.js
@@ -1,25 +1,50 @@
-
 playerList = document.querySelectorAll('.player-list button')
 for(const i of playerList){
     i.addEventListener('click',function (event) {
-        
-
-
         text = event.target.parentNode.childNodes[1].innerText
         playerSelect = document.getElementById('player-select')
 
         li = document.createElement('li')
         li.innerText = text
         
-
-        some = document.querySelectorAll('li')
-        if (some.length==5) {
+        listItem = document.querySelectorAll('li')
+        if (listItem.length==5) {
             alert('asdasdasd')
         }else{
             playerSelect.appendChild(li)
         }
-
-        
-
     })
 }
+
+document.getElementById('calculate-btn').addEventListener('click',function () {
+    perPlayerInput = document.getElementById('per-player-input')
+    perPlayerInputString = perPlayerInput.value
+    perPlayerInputValue = parseInt(perPlayerInputString)
+
+    listItem = document.querySelectorAll('li') 
+    perPlayer = perPlayerInputValue * listItem.length
+
+    playerExpenses = document.getElementById('player-expenses')
+    playerExpenses.innerText = perPlayer
+
+})
+
+document.getElementById('calculate-total-btn').addEventListener('click',function () {
+    playerExpenses = document.getElementById('player-expenses')
+    playerExpensesString = playerExpenses.innerText
+    playerExpensesAmount = parseInt(playerExpensesString)
+
+    managerFee = document.getElementById('manager-fee')
+    managerFeeString = managerFee.value
+    managerFeeValue = parseInt(managerFeeString)
+
+    coachFee = document.getElementById('coach-fee')
+    coachFeeString = coachFee.value
+    coachFeeValue = parseInt(coachFeeString)
+    
+    totalValue =  playerExpensesAmount + managerFeeValue + coachFeeValue
+
+    totalAmount = document.getElementById('total-amount')
+    totalAmount.innerText = totalValue
+
+})
